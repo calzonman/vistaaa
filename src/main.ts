69fbe6f729da5cc82a3c88f7db2b881353a029ba
calzonman <130697325+calzonman@users.noreply.github.com>
2023-11-08 19:46:07 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
-
-const app = await NestFactory.create(AppModule);
-app.enableCors();
-await app.listen(3000);
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
